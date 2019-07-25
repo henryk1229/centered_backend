afd90f98aded884d6c58dc57472dcb2768bcc081
henryk1229 <henryk1229@gmail.com>
2019-07-25 17:23:26 -0400

    fixed

--- app/controllers/api/v1/themes_controller.rb
+++ app/controllers/api/v1/themes_controller.rb
@@ -1,3 +1,21 @@
 class Api::V1::ThemesController < ApplicationController
 
+  def index
+    themes = Theme.all
+
+    render json: themes
+  end
+
+  def show
+    theme = Theme.find(theme_params)
+
+    render json: theme
+  end
+
+  private
+  
+  def theme_params
+    params.permit(:name)
+  end
+
 end
